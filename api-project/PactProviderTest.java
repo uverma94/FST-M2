f9e4966dc319700004fb5092fef085392e8530b0
Uday Verma <76985181+uverma94@users.noreply.github.com>
2021-06-02 13:48:47 +0530

    Pact Project

---
+++ api-project/PactProviderTest.java
@@ -0,0 +1,28 @@
+package liveproject;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import au.com.dius.pact.provider.junit.Provider;
+
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+
+@Provider("UserProvider")
+@PactFolder("target/pacts")
+public class PactProviderTest {
+	@BeforeEach
+	void before(PactVerificationContext context) {
+		// Set target for provider to send request to
+		context.setTarget(new HttpTestTarget("localhost", 8080));
+	}
+	@TestTemplate
+	@ExtendWith(PactVerificationInvocationContextProvider.class)
+	void pactTestTemplate(PactVerificationContext context) {
+		// Verify the interaction between Consumer and Provider
+		// using the contract generated in target/pacts
+		context.verifyInteraction();
+	}
+}
